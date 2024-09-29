12d6dadad49741b88332b6bfe1d1acb8332b33ff
faram <romanchykov.webdev@gmail.com>
2024-09-29 19:36:15 +0200

    create home page - Add component RideCard nod finish

--- components/RideCard.tsx
+++ components/RideCard.tsx
@@ -1,4 +1,4 @@
-import { View, Text } from "react-native";
+import { View, Text, Image } from "react-native";
 import { Ride } from "@/types/type";
 
 const RideCard = ({
@@ -16,7 +16,20 @@ const RideCard = ({
   ride: Ride;
 }) => {
   return (
-    <View>
+    <View className="flex flex-row items-center justify-center bg-white rounded-full shadow-sm shadow-neutral-300 mb-3">
+      <View className="flex flex-row items-center justify-between p-3">
+        <View className="flex flex-ro item-center justify-between">
+          <Image
+            source={{
+              uri: `https://maps.geoapify.com/v1/staticmap?style=osm-bright&width=600&height=400&center=lonlat:${destination_longitude},${destination_latitude}&zoom=14&apiKey=${process.env.EXPO_PUBLIC_GEOAPIFY_API_KEY}`,
+            }}
+            className="w-[80px] h-[90px]
+            {/*border-2*/}
+            {/*border-red-500*/}
+            "
+          />
+        </View>
+      </View>
       <Text className="text-3xl">{driver.first_name}</Text>
     </View>
   );
